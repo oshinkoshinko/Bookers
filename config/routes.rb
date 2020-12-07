6488d8d0d52886feb8670e0c1fbf12d760ee7650
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2020-12-07 09:05:20 +0000

    [Update] 3-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'top' => 'top#top'
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show'
+
+  resources :books
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
