10f9358331bd85e5d2526a8733ccf5baacf77be3
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2020-12-08 05:33:50 +0000

    [Update2] 3-8

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'books' => 'books#index'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'books#edit'
+  patch 'books/:id' => 'books#update'
+  delete 'books/:id' => 'books#destroy'
 
   resources :books
 
